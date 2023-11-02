eca9caa8a6e23c5f547b7f32d13d31c11505232a
flyons <forrestjlyons@gmail.com>
2023-11-02 09:29:43 -0600

    4th c

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,20 +7,67 @@ background-image: url('/woman.jpg'); /* Set the background image path */
 background-size: cover;
 background-position: center;
 opacity: .8; /* Set the opacity value here (0.0 to 1.0) */
-  height: 100vh;/* Set the background color */
+height: 100vh;
+display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 `;
 
-const Testimonial = styled.div`
-  /* Add your styles for the testimonial section */
+const AttentionDiv = styled.div`
+  text-align: center;
+  position: absolute;
+  top: 75%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
+const Heading5 = styled.h5`
+  font-size: 24px;
+  border-left: 2px solid #000;
+  border-right: 2px solid #000;
+  display: inline-block;
+  padding: 0 20px;
+`;
+const Heading1 = styled.h1`
+  font-size: 48px;
+  margin-top: 20px;
+`;
+
+const Paragraph = styled.p`
+  font-size: 14px;
+  margin-top: 30px;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+`;
+const Button = styled.button`
+  margin: 0 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+`;
+
 
 const HomePage = () => {
   return (
     <HomePageContainer>
-      <Testimonial>
-        {/* Add your testimonial content here */}
-      </Testimonial>
-      {/* Add any additional content for the homepage */}
+      <AttentionDiv>
+        <Heading5>Attention-Grabbing Heading</Heading5>
+        <Heading1>Big Bold Heading</Heading1>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </Paragraph>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </Paragraph>
+        <ButtonsContainer>
+          <Button>Book Now</Button>
+          <Button>Learn More</Button>
+        </ButtonsContainer>
+      </AttentionDiv>
     </HomePageContainer>
   );
 }
