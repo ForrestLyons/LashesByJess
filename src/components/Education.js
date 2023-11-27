c3994254d3e3a6a2542751d36ef5a4563903bb5c
ForrestLyons <forrestjlyons@gmail.com>
2023-11-27 12:38:38 -0700

    11th

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,20 +1,172 @@
 import React from 'react';
-import styled from 'styled-components';
-import  { createGlobalStyle } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+import HomePage from './HomePage';
+import ContactPage from './Contact';
+import ServicesPage from './Services';
 
 
-const GlobalStyle = createGlobalStyle`
-  body {
+const GlobalStyle = styled.div`
+  * {
     margin: 0;
     padding: 0;
-    background-color: #efd9d7;
-    color: white; // Set the default text color to white or adjust as needed
-    font-family: 'Your Preferred Font', sans-serif; // Add your preferred font
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
+
+  body {
+    display: flex;
+    background: #333;
+    justify-content: flex-end;
+    align-items: flex-end;
+    min-height: 100vh;
   }
 `;
 
+const animateWaves = keyframes`
+  0% {
+    background-position-x: 1000px;
+  }
+  100% {
+    background-position-x: 0px;
+  }
+`;
+
+const animate = keyframes`
+  0% {
+    background-position-x: -1000px;
+  }
+  100% {
+    background-position-x: 0px;
+  }
+`;
+
+const Footer = styled.footer`
+  position: relative;
+  max-width: 1075px;
+  background: #3586ff;
+  min-height: 100px;
+  padding: 20px 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  back
+`;
+
+const Waves = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+  flex-wrap: wrap;
+  z-index: 0;
+`;
+
+const Wave = styled.div`
+  position: absolute;
+  top: -100px;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  background: url("https://i.ibb.co/wQZVxxk/wave.png");
+  background-size: 900px 100px;
+`;
+
+const Wave1 = styled(Wave)`
+  z-index: 1000;
+  opacity: 1;
+  bottom: 0;
+  animation: ${animateWaves} 4s linear infinite;
+
+`;
+
+const Wave2 = styled(Wave)`
+  z-index: 999;
+  opacity: 0.5;
+  bottom: 10px;
+  animation: ${animate} 4s linear infinite !important;
+
+`;
+
+const Wave3 = styled(Wave)`
+  z-index: 1000;
+  opacity: 0.2;
+  bottom: 15px;
+  animation: ${animateWaves} 3s linear infinite;
+
+`;
+
+const Wave4 = styled(Wave)`
+  z-index: 999;
+  opacity: 0.7;
+  bottom: 20px;
+  animation: ${animate} 3s linear infinite;
+`;
+
+const SocialIcons = styled.ul`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+  flex-wrap: wrap;
+`;
+
+const SocialIconItem = styled.li`
+  list-style: none;
+`;
+
+const SocialIconLink = styled.a`
+  font-size: 2rem;
+  color: #fff;
+  margin: 0 10px;
+  display: inline-block;
+  transition: 0.5s;
+
+  &:hover {
+    transform: translateY(-10px);
+  }
+`;
+
+const Menu = styled.ul`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+  flex-wrap: wrap;
+`;
+
+const MenuItem = styled.li`
+  list-style: none;
+`;
+
+const MenuLink = styled.a`
+  font-size: 1.2rem;
+  color: #fff;
+  margin: 0 10px;
+  display: inline-block;
+  transition: 0.5s;
+  text-decoration: none;
+  opacity: 0.75;
+  font-weight: 300;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Copyright = styled.p`
+  color: #fff;
+  margin: 15px 0 10px 0;
+  font-size: 1rem;
+  font-weight: 300;
+`;
+
 const PageWrapper = styled.div`
-  max-width: 1000px;
+  max-width: 1075px;
   margin: 0 auto;
   padding: 16px;
   height: 100vh;
@@ -66,58 +218,7 @@ const EducationContent = styled.div`
   padding: 20px;
 `;
 
-const FooterWrapper = styled.footer`
-  position: relative;
-  background-color: #3c3c3c; /* Set the background color as needed */
-  color: white; /* Set the text color as needed */
-  padding: 20px; /* Adjust padding as needed */
-  text-align: center;
-`;
 
-const Waves = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100px; /* Adjust the height of the waves */
-`;
-
-const Wave = styled.div`
-  height: 100%;
-  width: 100%;
-  background: url('/wave.svg') repeat-x; /* Use an SVG image for the wave effect */
-`;
-
-const SocialIcons = styled.ul`
-  list-style: none;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px; /* Adjust margin as needed */
-`;
-
-const SocialIconItem = styled.li`
-  margin: 0 10px; /* Adjust margin as needed */
-`;
-
-const SocialIconLink = styled.a`
-  color: white; /* Set the color of the social icons */
-  font-size: 24px; /* Adjust font size as needed */
-  text-decoration: none;
-
-  &:hover {
-    color: #B76E79; /* Set the hover color as needed */
-  }
-`;
-
-
-
-
-
-const CopyRight = styled.p`
-  margin: 0;
-  font-size: 14px; /* Adjust font size as needed */
-`;
 
 const EducationPage = () => {
   return (
@@ -136,26 +237,44 @@ const EducationPage = () => {
           <p>We are a team​ of certified and licensed beauty industry professionals with one goal in mind: the wellbeing of our clients. We believe that everybody deserves to feel great about the way they look, and our variety of services is suitable for every need. We offer affordable, safe and painless treatments for people of all ages. Our team has been carefully selected and trained, and we only use industry-approved, state-of-the-art technologies. It will be our pleasure to meet – get in touch today!</p>
           <Heading3>My client's well being is first and foremost!</Heading3>
         </EducationContent>
-        <FooterWrapper>
-      <Waves>
-        <Wave />
-      </Waves>
-      <SocialIcons>
-        {/* Add your social icons and links here */}
-        <SocialIconItem>
-          <SocialIconLink href="/facebook.png">
-            <ion-icon name="logo-facebook"></ion-icon>
-          </SocialIconLink>
-        </SocialIconItem>
-        <SocialIconItem>
-          <SocialIconLink href="#">
-            <ion-icon name="logo-instagram"></ion-icon>
-          </SocialIconLink>
-        </SocialIconItem>
-      </SocialIcons>
-
-      <CopyRight>&copy;2021 Nadine Coelho | All Rights Reserved</CopyRight>
-    </FooterWrapper>
+        <Footer>
+        <Waves>
+          <Wave1 />
+          <Wave2 />
+          <Wave3 />
+          <Wave4 />
+        </Waves>
+        <SocialIcons>
+          <SocialIconItem>
+            <SocialIconLink href="">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </SocialIconLink>
+          </SocialIconItem>
+          <SocialIconItem>
+            <SocialIconLink href="#">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </SocialIconLink>
+          </SocialIconItem>
+          <SocialIconItem>
+            <SocialIconLink href="#">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </SocialIconLink>
+          </SocialIconItem>
+          <SocialIconItem>
+            <SocialIconLink href="#">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </SocialIconLink>
+          </SocialIconItem>
+        </SocialIcons>
+        <Menu>
+          <MenuItem><MenuLink href={HomePage}>Home</MenuLink></MenuItem>
+          <MenuItem><MenuLink href={EducationPage}>Education</MenuLink></MenuItem>
+          <MenuItem><MenuLink href={ContactPage}>Contact</MenuLink></MenuItem>
+          <MenuItem><MenuLink href={ServicesPage}>Services</MenuLink></MenuItem>
+          {/* <MenuItem><MenuLink href={}>Contact</MenuLink></MenuItem> */}
+        </Menu>
+        <Copyright>&copy;2021 Nadine Coelho | All Rights Reserved</Copyright>
+      </Footer>
       </PageWrapper>
     </div>
   );
