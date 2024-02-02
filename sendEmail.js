1b4cdcf0fb06c506a82746f453ef95c2240ee39f
ForrestLyons <forrestjlyons@gmail.com>
2024-02-02 11:05:40 -0700

    here we go

--- sendEmail.js
+++ sendEmail.js
@@ -3,32 +3,28 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (formData) => {
   // Create a transporter using your email service provider's settings
   const transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com", // Corrected the typo here
-    port: 587,
-    secure: false, // use SSL, alternative is port 587 with `secure: false`
+    service: 'gmail',
     auth: {
-        user:"forrestjlyons@gmail.com", // Make sure these environment variables are set
-        pass: "Ssskeet007@",
-    },
-    debug: true, // Show debug output
-    logger: true, // Log information in console
+      user: process.env.SMTP_USER, // Ensure these are set in your .env
+      pass: process.env.SMTP_PASSWORD, // App-Specific Password for Gmail
+    }
   });
 
   // Setup email data
   const mailOptions = {
-    from: "forrestjlyons@gmail.com", // Use the email from your environment variable
+    from: process.env.SMTP_USER, // Match this with the SMTP_USER
     to: formData.email,
     subject: formData.subject,
     text: `Name: ${formData.name}\nEmail: ${formData.email}\nMessage: ${formData.message}`,
   };
 
-  // Send email
+  // Attempt to send the email
   try {
     const info = await transporter.sendMail(mailOptions);
     console.log('Email sent: ' + info.response);
   } catch (error) {
     console.error('Error sending email:', error);
-    throw error;
+    throw error; // Consider handling this more gracefully
   }
 };
 
